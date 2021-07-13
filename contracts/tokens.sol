d4bd635b13459cb27f01a50e38d0ce111cc7b694
theavaragejoe <80569724+theavaragejoe@users.noreply.github.com>
2021-07-13 09:20:22 +0200

    First commit

---
+++ contracts/tokens.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract Link is ERC20 {
+
+constructor() ERC20("Chainlink", "LINK") {
+    _mint(msg.sender, 1000);
+}
+
+}
